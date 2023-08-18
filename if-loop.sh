9cdd9e09f1ecfff00e34ac4f5c70e1fdad9fed27
Rahul Goraksha <126165928+rahulgoraksha@users.noreply.github.com>
2023-08-18 14:22:48 +0530

    Create if-loop.sh

---
+++ if-loop.sh
@@ -0,0 +1,25 @@
+#create an Vi editor
+vim if-loop.sh
+
+#After running the Vi editor  --Press I (INSERT) to make changes in the editor
+
+#!/bin/bash
+
+name=rahul
+
+if [ $name = "rahul" ]
+then
+        echo "yes, rahul loggin"
+fi
+
+#After completing content just click on esc (escape) button and type :wq to save required changes in the editor
+
+#Change the shell file permission
+chmod 700 if-loop.sh
+
+#To run the shell file
+./if-loop.sh
+
+<<output
+will list all the files name present in that directory
+output
